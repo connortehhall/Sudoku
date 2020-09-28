6de13fc2216f49007ec45867088fc74947d959cb
connortehhall <connor.tehhall@hotmail.co.nz>
2020-09-29 12:11:06 +1300

    Initial Commit

---
+++ Sudoku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sudoku
+//
+//  Created by Connor Teh-Hall on 29/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
